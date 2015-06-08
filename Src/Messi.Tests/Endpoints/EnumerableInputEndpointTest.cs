daee7c5a708965bf3fa1b2d17bf449c94e6c200e
ajlopez <webmaster@ajlopez.com>
2015-06-08 07:03:00 -0300

    Enumerable input endpoint, first tests

---
+++ Src/Messi.Tests/Endpoints/EnumerableInputEndpointTest.cs
@@ -0,0 +1,41 @@
+﻿namespace Messi.Tests.Endpoints
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Messi.Endpoints;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class EnumerableInputEndpointTest
+    {
+        [TestMethod]
+        public void RunNoMessage()
+        {
+            EnumerableInputEndpoint endpoint = new EnumerableInputEndpoint(new object[] { });
+
+            Assert.IsNull(endpoint.NextMessage());
+        }
+
+        [TestMethod]
+        public void RunOneMessage()
+        {
+            EnumerableInputEndpoint endpoint = new EnumerableInputEndpoint(new object[] { "foo" });
+
+            Assert.AreEqual("foo", endpoint.NextMessage());
+            Assert.IsNull(endpoint.NextMessage());
+        }
+
+        [TestMethod]
+        public void RunManyMessages()
+        {
+            EnumerableInputEndpoint endpoint = new EnumerableInputEndpoint(new object[] { "foo", "bar", "baz" });
+
+            Assert.AreEqual("foo", endpoint.NextMessage());
+            Assert.AreEqual("bar", endpoint.NextMessage());
+            Assert.AreEqual("baz", endpoint.NextMessage());
+            Assert.IsNull(endpoint.NextMessage());
+        }
+    }
+}
---
+++ Src/Messi/Endpoints/EnumerableInputEndpoint.cs
@@ -0,0 +1,25 @@
+﻿namespace Messi.Endpoints
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class EnumerableInputEndpoint
+    {
+        private IEnumerator<object> enumerator;
+
+        public EnumerableInputEndpoint(IEnumerable<object> elements)
+        {
+            this.enumerator = elements.GetEnumerator();
+        }
+
+        public object NextMessage()
+        {
+            if (!this.enumerator.MoveNext())
+                return null;
+
+            return this.enumerator.Current;
+        }
+    }
+}
