f309ea0b891a957146c3e1b825be30f2216eceab
ajlopez <webmaster@ajlopez.com>
2015-06-12 06:31:40 -0300

    LamdbaOutputEndpoint first test

---
+++ Src/Messi.Tests/Endpoints/LambdaOutputEndpointTest.cs
@@ -0,0 +1,27 @@
+﻿namespace Messi.Tests.Endpoints
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Messi.Endpoints;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class LambdaOutputEndpointTest
+    {
+        [TestMethod]
+        public void SendMessage()
+        {
+            object message = "Foo";
+            object processed = null;
+
+            LambdaOutputEndpoint endpoint = new LambdaOutputEndpoint(obj => { processed = obj; });
+
+            endpoint.Send(message);
+
+            Assert.IsNotNull(processed);
+            Assert.AreSame(message, processed);
+        }
+    }
+}
---
+++ Src/Messi/Endpoints/LambdaOutputEndpoint.cs
@@ -0,0 +1,22 @@
+﻿namespace Messi.Endpoints
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class LambdaOutputEndpoint
+    {
+        Action<object> action;
+
+        public LambdaOutputEndpoint(Action<object> action)
+        {
+            this.action = action;
+        }
+
+        public void Send(object message)
+        {
+            this.action(message);
+        }
+    }
+}
