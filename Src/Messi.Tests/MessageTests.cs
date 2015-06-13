6312d4a42dd5b4f1da0600c28b8d8c34a4fe83e8
ajlopez <webmaster@ajlopez.com>
2015-06-13 08:20:20 -0300

    Message first tests

---
+++ Src/Messi.Tests/MessageTests.cs
@@ -0,0 +1,54 @@
+﻿namespace Messi.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MessageTests
+    {
+        [TestMethod]
+        public void GetBody()
+        {
+            Message message = new Message("Foo");
+
+            Assert.AreEqual("Foo", message.Body);
+        }
+
+        [TestMethod]
+        public void GetUnknownHeader()
+        {
+            Message message = new Message(null);
+
+            Assert.IsNull(message.Headers["Foo"]);
+        }
+
+        [TestMethod]
+        public void SetGetHeader()
+        {
+            Message message = new Message(null);
+
+            message.Headers["Foo"] = "Bar";
+
+            Assert.AreEqual("Bar", message.Headers["Foo"]);
+        }
+
+        [TestMethod]
+        public void CloneHeaders()
+        {
+            Message message = new Message(null);
+            message.Headers["Foo"] = "Bar";
+
+            Message message2 = new Message(null, message.Headers);
+            message2.Headers["Foo"] = "Baz";
+            message2.Headers["Answer"] = 42;
+
+            Assert.AreEqual("Bar", message.Headers["Foo"]);
+            Assert.IsNull(message.Headers["Answer"]);
+            Assert.AreEqual("Baz", message2.Headers["Foo"]);
+            Assert.AreEqual(42, message2.Headers["Answer"]);
+        }
+    }
+}
---
+++ Src/Messi/Message.cs
@@ -0,0 +1,60 @@
+﻿namespace Messi
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+
+    public class Message
+    {
+        private MessageHeaders headers = new MessageHeaders();
+        private object body;
+
+        public Message(object body)
+        {
+            this.body = body;
+            this.headers = new MessageHeaders();
+        }
+
+        public Message(object payload, MessageHeaders headers)
+        {
+            this.body = body;
+            this.headers = headers.Clone();
+        }
+
+        public object Body { get { return this.body; } }
+
+        public MessageHeaders Headers { get { return this.headers; } }
+
+        public class MessageHeaders
+        {
+            private IDictionary<string, object> values = new Dictionary<string, object>();
+
+            public object this[string name]
+            {
+                get
+                {
+                    if (this.values.ContainsKey(name))
+                        return this.values[name];
+
+                    return null;
+                }
+
+                set
+                {
+                    this.values[name] = value;
+                }
+            }
+
+            internal MessageHeaders Clone()
+            {
+                MessageHeaders headers = new MessageHeaders();
+
+                foreach (var key in this.values.Keys)
+                    headers.values[key] = this.values[key];
+
+                return headers;
+            }
+        }
+    }
+}
