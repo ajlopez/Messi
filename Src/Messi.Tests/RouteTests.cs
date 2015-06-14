0dab5555365049a7d507d4abeadc2e26eb9a3199
ajlopez <webmaster@ajlopez.com>
2015-06-14 10:29:49 -0300

    Route first test

---
+++ Src/Messi.Tests/RouteTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Messi.Tests
+{
+    using System;
+    using Messi.Endpoints;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class RouteTests
+    {
+        [TestMethod]
+        public void ProcessMessage()
+        {
+            Message processed = null;
+
+            Route route = new Route();
+
+            route.From(new EnumerableInputEndpoint(new object[] { 42 }))
+                .To(new LambdaOutputEndpoint(msg => { processed = msg; }));
+
+            route.ProcessMessage();
+
+            Assert.IsNotNull(processed);
+            Assert.AreEqual(42, processed.Body);
+        }
+    }
+}
---
+++ Src/Messi/Route.cs
@@ -0,0 +1,31 @@
+﻿namespace Messi
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using Messi.Endpoints;
+
+    public class Route
+    {
+        IInputEndpoint input;
+        IOutputEndpoint output;
+
+        public Route From(IInputEndpoint endpoint)
+        {
+            this.input = endpoint;
+            return this;
+        }
+
+        public Route To(IOutputEndpoint endpoint)
+        {
+            this.output = endpoint;
+            return this;
+        }
+
+        public void ProcessMessage()
+        {
+            this.output.Send(this.input.NextMessage());
+        }
+    }
+}
